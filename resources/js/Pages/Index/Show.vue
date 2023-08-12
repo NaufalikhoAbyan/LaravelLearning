651af7d0930629cb5ef42b74146724b85225f53f
NaufalikhoAbyan <apradana448@gmail.com>
2023-08-12 15:05:45 +0700

    Routes $ Passing data

---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Show</div>
+    <Link href="/">Index</Link>
+    <div>
+        The message is {{ message }}
+    </div>
+</template>
+
+<script setup>
+    import {Link} from '@inertiajs/vue3';
+    defineProps([
+        'message'
+    ]);
+</script>
