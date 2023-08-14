a50cd7c63c89fa1151e4bf462e583e6e18f76919
NaufalikhoAbyan <apradana448@gmail.com>
2023-08-14 20:32:33 +0700

    Custom Components

---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div v-for="listing in listings">
+        <Link :href="`/listing/${listing.id}`"><ListingAddress :listing="listing"/></Link>
+    </div>
+</template>
+
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+    import ListingAddress from '../../Components/listingAddress.vue';
+    defineProps([
+        'listings'
+    ])
+</script>
---
+++ resources/js/Pages/Listing/Show.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <ListingAddress :listing="listing"/>
+    </div>
+</template>
+
+<script setup>
+    import ListingAddress from '../../Components/listingAddress.vue';
+
+    defineProps([
+        'listing'
+    ])
+</script>
