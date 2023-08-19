498d926e86f01a748ab65a6a4e4fead036ad9da6
NaufalikhoAbyan <apradana448@gmail.com>
2023-08-19 15:31:33 +0700

    Flash message done

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,15 +1,24 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
-    <div>THe page with time {{ timer }}</div>
-
+    <!-- <div>THe page with time {{ timer }}</div> -->
+    <div v-if="$page.props.flash.success" class="success">
+        {{ $page.props.flash.success }}
+    </div>
     <slot></slot>
 </template>
 
 <script setup>
     import { Link } from '@inertiajs/vue3';
-    import { ref } from 'vue';
+    // import { ref } from 'vue';
 
-    const timer = ref(0);
-    setInterval(() => timer.value++, 1000)
+    // const timer = ref(0);
+    // setInterval(() => timer.value++, 1000)
 </script>
+
+<style scoped>
+  .success {
+    background-color: green;
+    color: white;
+  }
+</style>
