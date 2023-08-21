d83d5276171f8c2a6f4b342283d5bd69e772574a
NaufalikhoAbyan <apradana448@gmail.com>
2023-08-21 22:31:30 +0700

    Price added to listings

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -3,6 +3,7 @@
         <Box v-for="listing in listings">
             <div>
                 <Link :href="`/listing/${listing.id}`">
+                    <Price :price="listing.price" class="text-2xl font-bold"/>
                     <ListingSpace :listing="listing" class="text-lg"/>
                     <ListingAddress :listing="listing" class="text-gray-500"/>
                 </Link>
@@ -20,6 +21,7 @@
     import ListingAddress from '../../Components/listingAddress.vue';
     import Box from '../../Components/UI/Box.vue';
     import ListingSpace from '../../Components/ListingSpace.vue';
+    import Price from '../../Components/Price.vue';
     defineProps([
         'listings'
     ])
