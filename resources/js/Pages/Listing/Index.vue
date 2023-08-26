bceaf6529c74b2d2744481405756bec11e8da669
NaufalikhoAbyan <apradana448@gmail.com>
2023-08-26 10:36:02 +0700

    Default root path for js created

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-        <Box v-for="listing in listings">
+        <Box v-for="listing in listings" :key="listing.id">
             <div>
                 <Link :href="`/listing/${listing.id}`">
                     <Price :price="listing.price" class="text-2xl font-bold"/>
@@ -18,10 +18,10 @@
 
 <script setup>
     import { Link } from '@inertiajs/vue3';
-    import ListingAddress from '../../Components/listingAddress.vue';
-    import Box from '../../Components/UI/Box.vue';
-    import ListingSpace from '../../Components/ListingSpace.vue';
-    import Price from '../../Components/Price.vue';
+    import ListingAddress from '@/Components/listingAddress.vue';
+    import Box from '@/Components/UI/Box.vue';
+    import ListingSpace from '@/Components/ListingSpace.vue';
+    import Price from '@/Components/Price.vue';
     defineProps([
         'listings'
     ])
