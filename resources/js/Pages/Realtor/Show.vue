33eddf794d3ac5632dd909ac286d8a5603c80074
NaufalikhoAbyan <apradana448@gmail.com>
2023-11-15 08:28:10 +0700

    Added offer view page

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -1,3 +1,42 @@
 <template>
-    Hello!
+    <div class="mb-4">
+        <Link :href="`/realtor/listing`">← Go back to listings</Link>
+    </div>
+    <section class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+        <Box v-if="!hasOffers" class="flex md:col-span-7 items-center">
+            <div class="w-full text-center font-medium text-gray-500">
+                No Offers
+            </div>
+        </Box>
+        <div v-else class="md:col-span-7">
+            <offer class="mb-4" v-for="offer in props.listing.offers" :offer="offer" :listing-price="props.listing.price"/>
+        </div>
+
+        <Box class="col-span-5 h-fit">
+            <template #header>
+                Basic info
+            </template>
+            <Price :price="listing.price" class="text-2xl font-bold"/>
+            <ListingSpace :listing="listing" class="text-lg"/>
+            <ListingAddress :listing="listing" class="text-gray-500"/>
+        </Box>
+    </section>
 </template>
+
+<script setup>
+    import {Link} from '@inertiajs/vue3';
+    import Box from "@/Components/UI/Box.vue";
+    import Price from "@/Components/Price.vue";
+    import ListingSpace from "@/Components/ListingSpace.vue";
+    import ListingAddress from "@/Components/listingAddress.vue";
+    import {computed} from "vue";
+    import Offer from "@/Pages/Realtor/Show/Components/Offer.vue";
+
+    const props = defineProps({
+        listing: Object
+    })
+
+    const hasOffers = computed(() =>{
+        return props.listing.offers.length
+    })
+</script>
---
+++ resources/js/Pages/Realtor/Show/Components/Offer.vue
@@ -0,0 +1,36 @@
+<template>
+    <Box>
+        <template #header>Offer #{{props.offer.id}}</template>
+        <div class="flex justify-between items-center">
+            <div>
+                <Price :price="props.offer.amount" class="text-xl"/>
+                <div class="text-gray-500">Difference <Price :price="difference"/></div>
+                <div class="text-gray-500 text-sm">Made by John Doe</div>
+                <div class="text-gray-500 text-sm">Made on {{madeOn}}</div>
+            </div>
+            <div>
+                <Link class="btn-outline text-xs font-medium" as="button">Accept</Link>
+            </div>
+        </div>
+    </Box>
+</template>
+
+<script setup>
+    import Box from "@/Components/UI/Box.vue";
+    import Price from "@/Components/Price.vue";
+    import {computed} from "vue";
+    import {Link} from "@inertiajs/vue3";
+
+    const props = defineProps({
+        offer: Object,
+        listingPrice: Number
+    })
+
+    const difference = computed(() => {
+        return props.offer.amount - props.listingPrice
+    })
+
+    const madeOn = computed(() => {
+        return new Date(props.offer.created_at).toLocaleDateString()
+    })
+</script>
